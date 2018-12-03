420d572634fa8a7957b119e23c405a359da1d09f
Daniel Donohue <ddonohue@earnestresearch.com>
2018-12-03 11:26:06 -0500

    multiple-cursors

--- emacs.d/personal/personal.el
+++ emacs.d/personal/personal.el
@@ -14,3 +14,10 @@
 
 ;; Turn on line number gutters
 (global-linum-mode t)
+
+;; Multiple cursors
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
